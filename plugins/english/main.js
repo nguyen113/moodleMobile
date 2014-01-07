2165fd43eab8fcf45dc80681e45d72fdb17c6e4e
nguyen113 <nguyen113@gmail.com>
2014-01-07 23:42:22 +0700

    Add Comments to main.js

--- plugins/english/main.js
+++ plugins/english/main.js
@@ -8,6 +8,14 @@ var templates = [
 
 define(templates, function (listeningVideo, listeningQuestion, reading, manager, langStringEN) {	
 	var plugin = {
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//Setting co ban cua plugin
+		//name: ten cua plugin, dung de goi cac function va doi tuong trong no. Vidu: MM.plugin.english.listenToVideo(1234)
+		//type: loai plugin, de general de debug, sau nay doi thanh course
+		//subMenus: cac menu con, truyen bien thong qua cac menu nay va action
+		//lang: ngon ngu, can nghien cuu them
+		//toogler: bat nut toggle de xo ra submenu
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         settings: {
             name: "english",
             type: "general",
@@ -24,38 +32,42 @@ define(templates, function (listeningVideo, listeningQuestion, reading, manager,
             toogler: true
         },
         
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//Cau hinh navigate trang khi bam vao submenu
+		//Lien quan toi settings.subMenus
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         routes: [
             ["english/listening", "english_listen", "listenToVideo"],
             ["english/reading", "english_read", "readingPassage"],
             ["english/manager", "english_manager", "managerCourse"],
         ],
         
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//ham mo giao dien video theo videoID cua youtube, dang bi loi, phai mo 2 lan moi hien
+		//goi ham bang MM.plugin.english.listenToVideo(id)
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         listenToVideo: function(videoID) {
+		
+			//The hien trang thai de debug
             MM.log('Navigate to Listening page', 'english');
+			
+			//Chuyen toi trang dich
             MM.Router.navigate("");
+			
+			//hien thi loading icon o panel center
             MM.panels.showLoading('center');
+			
+			//tao doi tuong template, gom cac bien truyen vao trang dich
 			var tpl = {
 				videoID: videoID
 			}
 			
-			
-			
-			
+			//render trang listeningVideo voi cac bien tu tpl
 			var html = MM.tpl.render(MM.plugins.english.templates.listeningVideo.html, tpl);
             if (MM.deviceType == "tablet") {
 				MM.panels.html('right', '');
             }			
-			MM.panels.show("right", html); 	
-			
-			
-			
-			
-			
-			
-
-
-
-			
+			MM.panels.show("right", html); 				
         },
 		
 		addQuestionToPanel: function(questionID, panel){
